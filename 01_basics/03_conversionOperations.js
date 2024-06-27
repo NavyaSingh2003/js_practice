472343412ffcd00c6ca1b322401081097bdeb1e1
NavyaSingh2003 <110404553+NavyaSingh2003@users.noreply.github.com>
2024-06-27 10:55:15 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,68 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);  //converting to 'number' datatype
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//aise hi we can do conversion into String(), Boolean(), Number()
+
+/**/ 
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);  //converting to 'number' datatype
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// /**/ 
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);  //converting to 'number' datatype
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// /**/ 
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);  //converting to 'number' datatype
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// /**/ 
+// let score = "navya"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);  //converting to 'number' datatype
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*Conversion into number datatype*/
+// "33" => 33
+// "33ac" => NaN
+// true => 1, false => 0
+
+
+/* */
+let isLoggedIn = "navya"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "navya" => true
+

487844572bd193263f7bc9f13f7b5faf70723b2c
NavyaSingh2003 <110404553+NavyaSingh2003@users.noreply.github.com>
2024-06-27 18:01:01 +0000

    num and maths

--- 01_basics/08_nums_maths.js
+++ 01_basics/08_nums_maths.js
@@ -9,7 +9,7 @@ const balance = new Number(100)
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));  //learn precision by changing value and prepare for interview
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));

50ae994aaaed0b3826f15e40c659680d6900609c
NavyaSingh2003 <110404553+NavyaSingh2003@users.noreply.github.com>
2024-06-27 11:18:00 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,36 @@
+// ********************operations******************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "navya"
+let str3 = str1 + str2;
+//console.log(str3);
+
+/* see their outputs too */
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+//code readability is must so use parenthesis wherever needed
+// console.log((3 + 4) * (5 % 3));
+
+console.log(+true);  // 1
+console.log(+"");  // 0 
+
+
+//read prefix and postfix operator from MDN
+let counter = 100
+++counter;
+console.log(counter); 

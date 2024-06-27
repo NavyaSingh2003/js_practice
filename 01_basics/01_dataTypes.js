22b7be75e34b1b3d33a4e1c9fb29a2214b9e8e8f
NavyaSingh2003 <110404553+NavyaSingh2003@users.noreply.github.com>
2024-06-27 09:41:49 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3)  //we are using nodejs, not browser so it won't work
+
+console.log(3 + 3)
+//code readability shd be high
+console.log("Navya")
+
+// Documentations to be kept in mind for js -> tc39, mdn
+
+let name = "navya"
+let age = 18
+let isLoggedIn = false
+
+/* Datatypes */
+// number => 2 to power 53
+// bigint => used to represent big numbers
+// string => ""
+// boolean => true/false
+// null => standalone value (datatype too)
+// undefined => 
+// symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof undefined); // output -> undefined
+console.log(typeof null);  // output -> object

04e7f8e8010f189215e1174b97842f001a1e2c1c
NavyaSingh2003 <110404553+NavyaSingh2003@users.noreply.github.com>
2024-06-28 10:34:16 +0000

    nums and maths

--- 01_basics/08_nums_maths.js
+++ 01_basics/08_nums_maths.js
@@ -1,14 +1,13 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100)  //if want to ensure 'number' datatype
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(2));  //decimal k baad kitne places chahiye
 
 const otherNumber = 123.8966
-
 // console.log(otherNumber.toPrecision(4));  //learn precision by changing value and prepare for interview
 
 const hundreds = 1000000
@@ -24,9 +23,10 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+/* prepare interview ques on this */ 
+console.log(Math.random());   //gives number between 0 and 1(including decimal)
+console.log((Math.random()*10) + 1);  //multiply by 10 isliye kia kyuki I want nos greater than 1
+console.log(Math.floor(Math.random()*10) + 1); 
 
 const min = 10
 const max = 20

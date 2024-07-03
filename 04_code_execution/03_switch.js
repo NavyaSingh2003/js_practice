b7b03a83e03d4b0d45a3fd90dbc9be4b895795b1
NavyaSingh2003 <110404553+NavyaSingh2003@users.noreply.github.com>
2024-07-03 14:15:19 +0000

    switch

---
+++ 04_code_execution/03_switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        //break;
+    case "feb":
+        console.log("feb");
+        //break;
+    case "march":
+        console.log("march");
+        //break;
+    case "april":
+        console.log("april");
+       // break;
+
+    default:
+        console.log("default case match");
+       // break;
+}
+
+// Q) agr 'break' statement nhi likhege toh jo case match hoga uske aage k sare cases execute hote jayge jb tk kisi case m 'break' nhi milega
